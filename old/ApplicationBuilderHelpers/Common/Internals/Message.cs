d79244f7ef9077360a71cb736c8c67aeba50d8cc
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2025-08-06 19:53:05 +0800

    Updated CICD (#139)

--- old/ApplicationBuilderHelpers/Common/Internals/Message.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ApplicationBuilderHelpers.Common.Internals;
-
-internal class Message
-{
-    internal const string RequiresUnreferencedCodeMessage = $"Serializable objects must preserve all its required types when trimming is enabled";
-}
--- old/ApplicationBuilderHelpers/Exceptions/NoConfigValueException.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace ApplicationBuilderHelpers.Exceptions;
-
-public class NoConfigValueException(string configName) : Exception($"{configName} config is empty")
-{
-}
